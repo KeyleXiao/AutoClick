4b849008ff368dcf0059640a2ccbd7a45a57a84a
Keyle Xiao <keyle_xiao@hotmail.com>
2025-06-05 23:11:10 +0800

    Add CLI workflow tool and Chinese instructions

---
+++ cli_workflow.py
@@ -0,0 +1,79 @@
+import json
+import base64
+import tempfile
+import os
+import time
+import argparse
+from KeyleFinderModule import KeyleFinderModule
+
+
+def load_items(config_path):
+    with open(config_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    items = []
+    for entry in data:
+        if 'image' in entry:
+            img_data = base64.b64decode(entry['image'])
+            tmp = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
+            tmp.write(img_data)
+            tmp.close()
+            path = tmp.name
+        else:
+            path = entry['path']
+        items.append({'path': path, 'double_click': entry.get('double_click', False)})
+    return items
+
+
+def cleanup_items(items):
+    for item in items:
+        path = item['path']
+        if path.startswith(tempfile.gettempdir()) and os.path.exists(path):
+            os.unlink(path)
+
+
+def run_workflow(items, debug=False, loop=False, interval=0.5):
+    import pyautogui
+    try:
+        while True:
+            screenshot = pyautogui.screenshot()
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as tmp:
+                screenshot.save(tmp.name)
+                screen_path = tmp.name
+            finder = KeyleFinderModule(screen_path)
+            for idx, item in enumerate(items):
+                result = finder.locate(item['path'], debug=debug)
+                if result.get('status') == 0:
+                    tl = result['top_left']
+                    br = result['bottom_right']
+                    center_x = (tl[0] + br[0]) // 2
+                    center_y = (tl[1] + br[1]) // 2
+                    pyautogui.moveTo(center_x, center_y)
+                    if item['double_click']:
+                        pyautogui.click(clicks=2)
+                    else:
+                        pyautogui.click()
+                    print(f'Item {idx} matched at {center_x},{center_y}')
+                else:
+                    print(f'Item {idx} match failed')
+            os.unlink(screen_path)
+            if not loop:
+                break
+            time.sleep(interval)
+    finally:
+        cleanup_items(items)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run AutoClick workflow from a JSON file')
+    parser.add_argument('config', help='JSON workflow file exported from the GUI')
+    parser.add_argument('--debug', action='store_true', help='show debug preview windows')
+    parser.add_argument('--loop', action='store_true', help='repeat workflow until interrupted')
+    parser.add_argument('--interval', type=float, default=0.5, help='delay between loops in seconds')
+    args = parser.parse_args()
+
+    items = load_items(args.config)
+    run_workflow(items, debug=args.debug, loop=args.loop, interval=args.interval)
+
+
+if __name__ == '__main__':
+    main()
